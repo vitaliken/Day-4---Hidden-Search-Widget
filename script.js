6cbdf5ec5b5b0db890e33f1cf67dde63c7e058fd
Vitaly Hutarau <vitaliken@gmail.com>
2023-05-06 16:15:05 +0300

    add script

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// addEventListener
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
